41aeeacd17d47a52035d64fbccd2c75fec0f9d2a
Aaryan Sinha Roy <62799551+AaryanSinhaRoy@users.noreply.github.com>
2021-07-29 21:22:40 +0530

    Add files via upload

--- index.py
+++ index.py
@@ -18,7 +18,16 @@ def home():
 def mealinfo(mealname,mealid):
     meal_info_data=requests.get("http://www.themealdb.com/api/json/v1/1/lookup.php?i="+mealid)
     meal_info=json.loads(meal_info_data.content)
-    return render_template("mealbox.html",meal_info=meal_info)
+    inglist=[]
+    measurelist=[]
+    for x in meal_info['meals']:
+        for y in range(1,20):
+            if len(x['strIngredient'+str(y)]) !=0:
+                inglist.append(x['strIngredient'+str(y)])
+            if len(x['strMeasure'+str(y)]) !=0:
+                measurelist.append(x['strMeasure'+str(y)])
+
+    return render_template("mealbox.html",meal_info=meal_info,mealname=mealname,inglist=inglist,measurelist=measurelist)
 
 @app.route("/categories/<catname>")
 def catinfo(catname):
